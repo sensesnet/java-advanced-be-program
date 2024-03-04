e1615f63d6283eaf36bf26e1e2e6d3c0a54e2dac
sensesnet <sensesnet@gmail.com>
2024-03-04 18:07:39 +0100

    MOD-4: Spring security

---
+++ fourth_be_module/src/main/java/com/epam/learn/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.epam.learn.repository;
+
+import com.epam.learn.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
